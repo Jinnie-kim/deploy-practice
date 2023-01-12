803e3b77f145719b171ec43c23784a3b5149e61f
Jinnie <hjk7639@gmail.com>
2023-01-12 20:57:00 +0900

    배포 후 문구 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="App">hello world</h1>;
+  return (
+    <>
+    <h1 className="App">hello world</h1>
+    <p>배포 성공!?</p>
+    </>
+  )
 }
 
 export default App;
